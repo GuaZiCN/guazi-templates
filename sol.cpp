d9fe4a0249e25b93f82bc97539c66e18caaa6f60
unknown <abhhcc@163.com>
2026-09-11 09:54:35 +0800

    just a "typical" sol.cpp.

---
+++ sol.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const bool _multitest=1;
+
+void solve()
+{
+
+}
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	int T;
+	if(_multitest) cin>>T;
+	else T=1;
+	while(T--) solve();
+	return 0;
+}
