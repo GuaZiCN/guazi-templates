5a2fde4132487b875fd025d3f301d3f16f824997
GuaZiCN <abhhcc@163.com>
2026-09-18 17:39:40 +0800

    add quick pow function.

--- math/lib.cpp
+++ math/lib.cpp
@@ -1,1 +1,18 @@
-ECHO 处于打开状态。
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int P=998244353;
+//const int P=1e9+7;
+
+int pow(int a,int x)
+{
+    int ans=1;
+    while(x)
+    {
+        if(x&1) ans=ll(ans)*a%P;
+        a=ll(a)*a%P;
+        x>>=1;
+    }
+    return ans;
+}
